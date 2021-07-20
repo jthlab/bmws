7f5f29e16bbb55507fd8d30abd43384a213d5834
Iain Mathieson <mathii@Iains-MacBook-Pro.local>
2021-07-20 18:58:35 -0400

    scripting

--- run_estimation.py
+++ run_estimation.py
@@ -1,9 +1,11 @@
 #Run analysis scripted
 import estimate, sim
-import argparse
+import argparse, gzip
 import matplotlib.pyplot as plt
 import numpy as np
 from betamix import sample_paths, BetaMixture
+from scipy.stats import beta
+import pdb
 
 ################################################################################
 
@@ -16,14 +18,16 @@ def parse_options():
     parser.add_argument("-e", dest='example', action='store_true', help="run a simulated example to test installation" )
     parser.add_argument('-m', '--meta', type=str, default="", help=
                         "meta-information file")
-    parser.add_argument('-g', '--geno', type=str, default="", help=
-                        "root for eigenstrat format input files")
-    parser.add_argument('-s', '--snps', type=str, default="", help=
-                        "list of subset of snps to run on")
+    parser.add_argument('-v', '--vcf', type=str, default="", help=
+                        "vcf input files")
     parser.add_argument('-o', '--out', type=str, default="", help=
-                        "root for output files")
+                        "output file")
     parser.add_argument('-l', '--lam', type=int, default=5, help=
                         "log10(lambda) to use; default 5")
+    parser.add_argument('-g', '--gen', type=float, default=29, help=
+                        "generation time in years")
+    parser.add_argument('-n', '--Ne', type=float, default=10000, help=
+                        "Effective population size")
 
     return parser.parse_args()
 
@@ -44,12 +48,132 @@ def example(options):
 
 ################################################################################
 
+def read_meta_information(options):
+    """
+    Read in the date information
+    First column, sample ID
+    Second column, date in years_BP
+    """
+    meta={}
+    maxdate=0
+    
+    with open(options.meta) as metafile:
+        for line in metafile:
+            if line[0]=="#":
+                continue
+            else:
+                bits=line[:-1].split()
+                gen=int(float(bits[1])/options.gen)
+                meta[bits[0]]=gen
+                maxdate=max(gen, maxdate)
+
+    meta={k:maxdate-v for k,v in meta.items()}
+
+    return(meta)
+
+################################################################################
+
+class vcf():
+    """
+    read vcf file
+    """
+    def __init__(self,  file):
+        """
+        Open the vcf file, find the header lines, and load the sample ids
+        """
+        if file[-3:]==".gz":
+            self.file=gzip.open(file, "rt")
+        else:
+            self.file=open(file, "r")
+
+        in_header=True
+        while in_header:
+            line=self.file.readline()
+            if line[:2]=="##":
+                continue
+            elif line[0]=="#":
+                self.ids=line[:-1].split()[9:]
+                in_header=False
+
+    def __iter__(self):
+        return(self)
+                
+    def __next__(self):
+        """
+        Iterate over remaining lines of file. 
+        """
+        map={"1/1":1, "0/0":0, "./.":None}
+        line=self.file.readline()
+
+        if not line:
+            self.file.close()
+            raise StopIteration
+        
+        bits=line[:-1].split()
+        snpinfo=bits[:5]
+        gt=[map[x] for x in bits[9:]]
+        return(snpinfo,gt)
+
+################################################################################
+
+def gt_to_obs(ids, gt, meta):
+    """
+    convert gt to an observation list of (Obs, Alt) counts
+    """
+    
+    maxgen=max(meta.values())
+    obs=[[0,0] for x in range(maxgen+1)]
+    for i,g in enumerate(gt):
+        if g!=None:
+            gen=meta[ids[i]]
+            obs[gen][0]+=1
+            obs[gen][1]+=g
+
+    obs=np.array(obs)
+    return(obs)
+
+################################################################################
+
+def makeprior(obs):
+    """
+    Beta prior
+    """
+    M = 200
+    prior = BetaMixture.uniform(M)
+    f = np.mean([x[1] / x[0] for x in obs if x[0] > 0])
+    b = 4
+    a = b * f / (1 - f)
+    dens = [beta.pdf((x + 1) / (M + 1), a, b) for x in range(M)]
+    total = np.sum(dens)
+    dens = [x / total for x in dens]
+    prior = prior._replace(log_c=np.log(dens))
+    return(prior)
+
+################################################################################
+
 def main(options):
     if options.example:
         print("Running example; ignoring all input parameters")
         example(options)
         return()
 
+    meta=read_meta_information(options)
+    data=vcf(options.vcf)
+    ids=data.ids
+
+    lam=10 ** options.lam
+    
+    for snpinfo,gt in data:
+        obs=gt_to_obs(ids, gt, meta)
+        Ne = np.full(len(obs) - 1, options.Ne)
+        prior=makeprior(obs)
+        res = estimate.estimate(obs, Ne, lam=lam, prior=prior)
+
+        sl1=np.sqrt(np.mean(res*res))
+        sl2=np.sqrt(np.mean((res-np.mean(res))**2))
+
+        print("\t".join(snpinfo+[str(round(sl1,6)), str(round(sl2,6))]))
+                
 ################################################################################
 
 if __name__=="__main__":
