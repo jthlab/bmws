2b1241c1b162764197e7803326a19a3b7f6d5bce
Jonathan Terhorst <terhorst@gmail.com>
2024-03-04 11:21:27 -0500

    fix warnings in test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,9 @@
 def test_cli_test(script_runner):
-    ret = script_runner.run("bmws", "test")
+    ret = script_runner.run(["bmws", "test"])
     assert ret.success
 
 
 def test_cli_analyze(script_runner):
     # TODO: make a non-trivial analysis
-    ret = script_runner.run("bmws", "analyze", "-h")
+    ret = script_runner.run(["bmws", "analyze", "-h"])
     assert ret.success
